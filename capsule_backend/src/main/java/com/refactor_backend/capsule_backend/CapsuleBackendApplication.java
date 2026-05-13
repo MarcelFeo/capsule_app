de947043056c6079e28c7020045cba06c4d19252
MarcelFeo <marcelfeo29@gmail.com>
2026-05-13 10:31:41 -0300

    refactor: start refactoring the backend

---
+++ capsule_backend/src/main/java/com/refactor_backend/capsule_backend/CapsuleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.refactor_backend.capsule_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsuleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsuleBackendApplication.class, args);
+	}
+
+}
---
+++ capsule_backend/src/test/java/com/refactor_backend/capsule_backend/CapsuleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refactor_backend.capsule_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsuleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
