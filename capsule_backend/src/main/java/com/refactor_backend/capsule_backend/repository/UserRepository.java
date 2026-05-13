663dfcecee8b53d0eb67708855d8772059b92534
MarcelFeo <marcelfeo29@gmail.com>
2026-05-13 19:52:30 -0300

    add: create UserService

--- capsule_backend/src/main/java/com/refactor_backend/capsule_backend/repository/UserRepository.java
+++ capsule_backend/src/main/java/com/refactor_backend/capsule_backend/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.refactor_backend.capsule_backend.repository;
 import com.refactor_backend.capsule_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
 }
--- capsule_backend/src/main/java/com/refactor_backend/capsule_backend/service/UserService.java
+++ capsule_backend/src/main/java/com/refactor_backend/capsule_backend/service/UserService.java
@@ -1,7 +1,12 @@
 package com.refactor_backend.capsule_backend.service;
 
+import com.refactor_backend.capsule_backend.domain.User;
+import com.refactor_backend.capsule_backend.dto.UserRequest;
+import com.refactor_backend.capsule_backend.dto.UserResponse;
 import com.refactor_backend.capsule_backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -12,6 +17,43 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public UserResponse registerUser(UserRequest request) {
+        if (request.email() != null && userRepository.existsByEmail(request.email())) {
+            throw new RuntimeException("Email address already registered");
+        }
+
+        User user = request.toEntity();
+        User userSaved = userRepository.save(user);
+        return UserResponse.fromEntity(userSaved);
+    }
+
+    public UserResponse getUserProfile(UUID userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return UserResponse.fromEntity(user);
+    }
+
+    public UserResponse updateUser(UUID id, UserRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (request.email() != null && !request.email().equals(user.getEmail()) && userRepository.existsByEmail(request.email())) {
+            throw new RuntimeException("Email address already registered");
+        }
+
+        request.toFill(user);
+        User newUser = userRepository.save(user);
+        return UserResponse.fromEntity(newUser);
+    }
+
+    public void deleteUser(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        userRepository.delete(user);
+    }
 
 
 }
