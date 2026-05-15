8533033d3427160593ea0ffa716ba1015654a4c5
MarcelFeo <marcelfeo29@gmail.com>
2026-05-15 20:38:27 -0300

    feat(medication): add main functions in MedicationService

--- capsule_backend/src/main/java/com/refactor_backend/capsule_backend/repository/MedicationRepository.java
+++ capsule_backend/src/main/java/com/refactor_backend/capsule_backend/repository/MedicationRepository.java
@@ -1,9 +1,16 @@
 package com.refactor_backend.capsule_backend.repository;
 
 import com.refactor_backend.capsule_backend.domain.Medication;
+import com.refactor_backend.capsule_backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MedicationRepository extends JpaRepository<Medication, UUID> {
+
+    boolean existsByUserAndName(User user, String name);
+
+    List<Medication> findAllByUserOrderByStartDateDesc(User user);
+
 }
--- capsule_backend/src/main/java/com/refactor_backend/capsule_backend/service/MedicationService.java
+++ capsule_backend/src/main/java/com/refactor_backend/capsule_backend/service/MedicationService.java
@@ -1,7 +1,66 @@
 package com.refactor_backend.capsule_backend.service;
 
+import com.refactor_backend.capsule_backend.domain.Medication;
+import com.refactor_backend.capsule_backend.domain.User;
+import com.refactor_backend.capsule_backend.dto.MedicationRequest;
+import com.refactor_backend.capsule_backend.dto.MedicationResponse;
+import com.refactor_backend.capsule_backend.repository.MedicationRepository;
+import com.refactor_backend.capsule_backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MedicationService {
+
+    public final MedicationRepository medicationRepository;
+    public final UserRepository userRepository;
+
+    public MedicationService(UserRepository userRepository, MedicationRepository medicationRepository) {
+        this.userRepository = userRepository;
+        this.medicationRepository = medicationRepository;
+    }
+
+    public MedicationResponse registerMedication(MedicationRequest request) {
+
+        if (medicationRepository.existsByUserAndName(request.user(), request.name())) {
+            throw new RuntimeException("User already has a medication with that name");
+        }
+
+        Medication medication = request.toEntity();
+        Medication medicationSaved = medicationRepository.save(medication);
+        return MedicationResponse.fromEntity(medicationSaved);
+    }
+
+    public List<Medication> getMedicationsByUser(UUID userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+
+        return medicationRepository.findAllByUserOrderByStartDateDesc(user);
+    }
+
+    public MedicationResponse updateMedication(UUID medicationId, MedicationRequest request) {
+
+        Medication medication = medicationRepository.findById(medicationId)
+                .orElseThrow(() -> new RuntimeException("Medication not found"));
+
+        if (medicationRepository.existsByUserAndName(request.user(), request.name())) {
+            throw new RuntimeException("User already has a medication with that name");
+        }
+
+        request.toFill(medication);
+        Medication newMedication = medicationRepository.save(medication);
+        return MedicationResponse.fromEntity(newMedication);
+    }
+
+    public void deleteMedication(UUID medicationId) {
+        Medication medication = medicationRepository.findById(medicationId)
+                .orElseThrow(() -> new RuntimeException("Medication not found"));
+
+        medicationRepository.delete(medication);
+    }
+
 }
